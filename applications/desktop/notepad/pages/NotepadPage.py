a98e8f64b7892132ef6635f42ccb23f4dd04a5ea
CROWLEY\rosabjo <johan.rosabal@crowley.com>
2024-09-03 19:47:37 -0600

    Updates Desktop Apps

--- applications/desktop/notepad/pages/NotepadPage.py
+++ applications/desktop/notepad/pages/NotepadPage.py
@@ -1,26 +1,30 @@
-from core.ui.common.BasePage import BasePage
 from core.ui.driver.DriverManager import DriverManager
 from appium.webdriver.common.appiumby import AppiumBy as By
+from core.config.config_loader import load_desktop_config
 import time
+from core.config.config_cmd import get_profile
+
+
+def config_yaml():
+    profile = get_profile() or "qa"
+    # Load Profile Configurations
+    return load_desktop_config(f"../config/{profile}_config.yaml")
 
 
 class NotepadPage:
-
-    application = "C:\\Windows\\System32\\notepad.exe"
-    driver = None  # Esto se inicializará en el método get_instance
+    driver = DriverManager.windows_pc(config_yaml().desktop.application)
 
     # Locator
     __text_editor = (By.CLASS_NAME, "Edit")
     __menu_file = (By.CLASS_NAME, "Edit")
+    __dont_save_button = (By.NAME, "Don't Save")
 
     _instance = None
 
     @classmethod
     def get_instance(cls):
         if not cls._instance:
-            cls.driver = DriverManager.windows_pc(cls.application)
-            time.sleep(3)
-            cls._instance = NotepadPage()  # Ahora crea una instancia de NotepadPage
+            cls._instance = NotepadPage()
         return cls._instance
 
     def menu_edit(self):
@@ -35,6 +39,16 @@ class NotepadPage:
             self.driver.find_element(by, value).send_keys(text)
         return self
 
-    def close(self):
+    def quit(self):
         if self.driver:
             self.driver.quit()
+
+    def close(self):
+        if self.driver:
+            self.driver.close()
+            time.sleep(1)
+            try:
+                # try to click "Don't Save" if dialog box appears
+                self.driver.find_element(*self.__dont_save_button).click()
+            except:
+                pass
--- applications/desktop/notepad/tests/test_Notepad.py
+++ applications/desktop/notepad/tests/test_Notepad.py
@@ -1,10 +1,10 @@
-import pytest
-
+from core.utils.decorator import test
 from applications.desktop.notepad.pages.NotepadPage import NotepadPage
 
 
 class TestNotepad:
 
+    @test(test_case_id="API-0001", test_description="Test Notepad Windows Application.")
     def test_add_some_test(self):
         (
             NotepadPage.get_instance()
