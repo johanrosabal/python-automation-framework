02f51effb945f9bf1342f75105fde4b7b41d88af
CROWLEY\rosabjo <johan.rosabal@crowley.com>
2024-09-04 14:36:52 -0600

    Delete Files...

--- applications/web/softship/pages/MasterDataPage.py
+++
@@ -1,9 +0,0 @@
-from core.ui.common.BasePage import BasePage
-
-
-class MasterDataPage(BasePage):
-
-    def __init__(self, driver=None):
-        super().__init__()
-        if driver:
-            self.driver = driver
